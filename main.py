f526868eccdd0687d04b19046526b6718ce5d5fd
Susana Aguilera <susy.aguilera@outlook.com>
2024-06-06 12:08:02 -0600

    dynamic query

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from libs.cloudant import cloudantDBClass
+
 import json
 import os
 
 class cloudant2Excel:
     cloudant = cloudantDBClass()
-
+    with open('queries/getAssessments.json') as json_file:
+        queries = json.load(json_file)
     dbCred = json.loads(os.getenv("DBSOURCE"))
 
     if (cloudant.loadCredentials(dbCred) and cloudant.connectToCloudant()):
         cloudant.connectToDB(dbCred["NAMEDB"])
         print("Successfully connected!")
-
-        query = {'IOT': 'IBM Americas'}
             
-        
-        data = cloudant.queryDocument("testing", query)
-        print(len(data))
+        for query in queries:
+            data = cloudant.queryDocument("get data", queries[query]["selector"])
+            print(len(data))
         
 
         

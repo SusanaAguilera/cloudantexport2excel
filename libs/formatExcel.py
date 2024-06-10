c4c1e768222c494674c6aa82811649d3ce107fb9
Ivan Warlock <Ivan.Warlock@ibm.com>
2024-06-10 14:36:12 -0600

    POC wroking

---
+++ libs/formatExcel.py
@@ -0,0 +1,78 @@
+# -----------------------------------------------------------
+# class to put format on a excel file using pandas
+#
+# (C) 2024 Ivan Gustavo Ortiz García
+# Released under MIT License (MIT)
+# GH Repository https://github.com/SusanaAguilera/cloudantexport2excel 
+# -----------------------------------------------------------
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class excelFormatting:
+    """
+        excelFormatting used put some format on excel files.
+        
+        Attributes:
+            writer [dict]
+            worksheet [dict]
+        Methods:
+            createWriter(self, filename="", sheetName="",dataFrame={}): Creates an Excel File and setup the writer.\n
+            setSheet(self, sheetName=""): Set the Sheet to be edited.\n
+            setHeaderFormat(self, headerFormat={}, dataFrame={}): Set the Header's format from a target sheet.\n
+            closeWriter(self): Close the writer to Stop the editing.
+    """ 
+    
+    writer={};
+    worksheet={};
+    workbook={};
+        
+    def createWriter(self, filename="", sheetName="",dataFrame={}):
+        """
+        """
+        try:
+            self.writer = pd.ExcelWriter(f"{filename}.xlsx", engine="xlsxwriter");
+            dataFrame.to_excel(self.writer, sheet_name=sheetName, startrow=0, header=True)
+            return True;
+        except Exception as e:
+            print('Error at [libs][excelFormatting][createWriter].-')
+            print(e)
+            return (False);
+        
+    def setSheet(self, sheetName=""):
+        """
+        """
+        try:
+            self.workbook = self.writer.book;
+            self.worksheet = self.writer.sheets[sheetName];
+        except Exception as e:
+            print('Error at [libs][excelFormatting][setSheet].-')
+            print(e)
+            return (False);
+    
+    def setHeaderFormat(self, headerFormat={}, dataFrame={}):
+        """
+        """
+        try:
+            header_format = self.workbook.add_format(headerFormat);
+            # Write the column headers with the defined format.
+            for col_num, value in enumerate(dataFrame.columns.values):
+                self.worksheet.write(0, col_num + 1, value, header_format)    
+        except Exception as e:
+            print('Error at [libs][excelFormatting][setHeaderFormat].-')
+            print(e)
+            return (False);
+        
+    def closeWriter(self):
+        """
+        """
+        try:
+            self.writer.close();
+            return True;
+        except Exception as e:
+            print('Error at [libs][excelFormatting][closeWriter].-')
+            print(e)
+            return (False);
+        
